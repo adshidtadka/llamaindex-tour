94af7538f78a9203ea1b2e462487b88ca12e2bb2
adshidtadka <nextage900fx@gmail.com>
2023-12-16 17:10:43 +0900

    remove unnecessary

--- starter.py
+++ starter.py
@@ -24,8 +24,6 @@ else:
     storage_context = StorageContext.from_defaults(persist_dir="./storage")
     index = load_index_from_storage(storage_context)
 
-documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
 
 query_engine = index.as_query_engine()
 response = query_engine.query("What did the author do growing up?")
